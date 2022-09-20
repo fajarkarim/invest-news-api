91446fc8280690be3bd5d012711b7c2944385807
Fajar Abdul Karim <fajarabdulkarim@gardakus-MacBook-Air.local>
2022-09-20 13:36:06 +0700

    add error handing when upload to csv

--- scripts/upload_csv_to_bq.py
+++ scripts/upload_csv_to_bq.py
@@ -4,31 +4,34 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/root/gcloud_credential/stoked-brand-360411-bc40ff939c35.json"
 
 def uploadCsv(**kwargs):
-    csvFileName = kwargs["csvName"]
-    TABLE_NAME = "COBA_AJA"
-    client = bigquery.Client()
+    try:
+        csvFileName = kwargs["csvName"]
+        TABLE_NAME = "COBA_AJA"
+        client = bigquery.Client()
 
-    TABLE_ID = f"stoked-brand-360411.web_scraping.{TABLE_NAME}"
+        TABLE_ID = f"stoked-brand-360411.web_scraping.{TABLE_NAME}"
 
-    job_config = bigquery.LoadJobConfig(
-        autodetect=True,
-        skip_leading_rows=1,
-        allow_quoted_newlines=True,
-        source_format=bigquery.SourceFormat.CSV,
-        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
-    )
+        job_config = bigquery.LoadJobConfig(
+            autodetect=True,
+            skip_leading_rows=1,
+            allow_quoted_newlines=True,
+            source_format=bigquery.SourceFormat.CSV,
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+        )
 
-    uri = f"gs://baby-pips-calendar-news/{csvFileName}"
+        uri = f"gs://baby-pips-calendar-news/{csvFileName}"
 
-    load_job = client.load_table_from_uri(
-        uri, TABLE_ID, job_config=job_config
-    )  # Make an API request.
+        load_job = client.load_table_from_uri(
+            uri, TABLE_ID, job_config=job_config
+        )  # Make an API request.
 
-    load_job.result()
+        load_job.result()
 
-    destination_table = client.get_table(TABLE_ID)
-    print("Loaded {} rows.".format(destination_table.num_rows))
-    return True
+        destination_table = client.get_table(TABLE_ID)
+        print("Loaded {} rows.".format(destination_table.num_rows))
+        return True
+    except:
+        return False    
 
 if __name__ == "__main__":
     uploadCsv(csvName=sys.argv[1])
